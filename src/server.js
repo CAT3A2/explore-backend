e16750db00e0eca3500b33bf973e872dc24610f4
AvelyV <avely.voore@gmail.com>
2022-02-10 20:45:29 +1000

    deleted cors options

--- src/server.js
+++ src/server.js
@@ -15,15 +15,10 @@ const postRouter = require("./routes/post");
 const profileRouter = require("./routes/profile");
 
 // const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
 
-// app.use(cors());
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/auth", authRouter);
