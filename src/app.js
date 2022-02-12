3a7ad041abdeb06e965a84e6e5b22b61b079ebfd
AvelyV <avely.voore@gmail.com>
2022-02-12 15:31:00 +1000

    reapplied corsOptions

--- src/app.js
+++ src/app.js
@@ -7,13 +7,18 @@ const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
 const profileRouter = require("./routes/profile");
 
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: "http://localhost:3000",
+  // credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.get("/", (req, res) => {
@@ -24,6 +29,6 @@ app.get("/", (req, res) => {
 app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.use("/profile", profileRouter);
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 module.exports = app;
