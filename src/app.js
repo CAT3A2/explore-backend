c5e0ca604f2f2336938f5ac3cfb4503b537ec26a
Shane Qi <shane_qi@hotmail.com>
2022-02-12 15:19:29 +1000

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -7,19 +7,15 @@ const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
 const profileRouter = require("./routes/profile");
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 app.get("/", (req, res) => {
   res.json({
     version: version,
@@ -28,6 +24,6 @@ app.get("/", (req, res) => {
 app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.use("/profile", profileRouter);
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 module.exports = app;
