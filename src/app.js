31a48f7a08c27f3b0f9cf643f1f4109bf1c43c62
Shane Qi <shane_qi@hotmail.com>
2022-02-13 15:02:07 +1000

    remove cors option

--- src/app.js
+++ src/app.js
@@ -7,12 +7,6 @@ const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
 const profileRouter = require("./routes/profile");
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  // credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -24,6 +18,5 @@ app.get("/", (req, res) => {
 app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.use("/profile", profileRouter);
-app.use(cors(corsOptions));
 
 module.exports = app;
